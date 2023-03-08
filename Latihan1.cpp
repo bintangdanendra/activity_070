3e846e57a87f49abcb569558bce82bce6cb63080
BintangDanendra <bintang.danendra140704@gmail.com>
2023-03-08 12:23:54 +0700

    first commit

---
+++ Latihan1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main (){ //begin
+
+//Numeric Luas, P, L
+double Luas, P, L;
+//Display "Masukan Panjangnya = "
+cout << "Masukan Panjangnyan = ";
+//Accept P
+cin >> P;
+//Display "Masukan Lebarnya = "
+cout << "Masukan Lebarnya = ";
+//Accept L
+cin >> L;
+//Compute Luas = P*L
+Luas = P*L;
+//Display "Luasnya = " + Luas
+cout << "Luasnya = " << Luas;
+}//End
---
+++ Latihan3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+double P, L;
+
+void input(){
+    cout << "Masukang Panjangnya = ";
+    cin >> P;
+    cout << "Masukan Lebarnya = ";
+    cin >> L;
+}
+
+double proses(){
+    return P*L;
+}
+
+double proses2(double a, double b){
+    return a*b;
+}
+
+void output(){
+    cout << "Luasnya = " << proses();
+    cout << "\nLuasnya ke 2 = " << proses2(P,L);
+}
+    
+int main (){
+    input();
+    output();
+}
